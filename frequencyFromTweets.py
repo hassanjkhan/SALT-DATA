dbab968b5da92995fd966484433680e82a2545ff
Hassan Khan <hassanjkhan6@gmail.com>
2022-01-19 09:20:51 -0500

    updating frequencyfromtweets

--- frequencyFromTweets.py
+++ frequencyFromTweets.py
@@ -1,29 +1,27 @@
 import pandas as pd
 
-for i in range(5,7):
-    print(i)
-    df = pd.read_csv("csvFiles/topTweetsofMonthBear"+str(i)+".csv")
-    # create dataframe 
-    cols = ['Word', 'Frequency']
-    data = {}
-    for index, row in df.iterrows():
-        tweet = row['tweet']
-        listOfWords = tweet.split()
-        for wordIndex in range(0,len(listOfWords)-2):
-            doubleWord = listOfWords[wordIndex] + " " + listOfWords[wordIndex + 1] + " "+ listOfWords[wordIndex + 2]
-            if doubleWord in data:
-                data[doubleWord] = data[doubleWord] + 1
-            else:
-                data[doubleWord] = 1
-            wordIndex += 1
+df = pd.read_csv("csvFiles/topBullTweets/topBullTweets.csv")
+# create dataframe 
+cols = ['Word', 'Frequency']
+data = {}
+for index, row in df.iterrows():
+    tweet = row['tweet']
+    listOfWords = tweet.split()
+    for wordIndex in range(0,len(listOfWords)):
+        word = listOfWords[wordIndex] #+ " " + listOfWords[wordIndex + 1] + " " + listOfWords[wordIndex + 2]  + " " + listOfWords[wordIndex + 3]+ " " + listOfWords[wordIndex + 4] +  " " + listOfWords[wordIndex + 5]
+        if word in data:
+            data[word] = data[word] + 1
+        else:
+            data[word] = 1
+        wordIndex += 1
 csvData = []
 for word in data:
-    if data[word] > 5:
+    if data[word] > 0:
         print(word + " -> freq: " + str(data[word])) 
     csvData.append([word, data[word]])
 
 df = pd.DataFrame(csvData, columns=cols)
 
-df.to_csv('topBearTripleWords.csv')
+df.to_csv('csvFiles/topBullTweets/topBullWords.csv')
 
 
