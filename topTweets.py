5f953184de0dac2dbb1e3fd38fdbbff6c44f747b
Hassan Khan <hassanjkhan6@gmail.com>
2022-04-04 12:08:54 -0400

    SBERT Work

--- topTweets.py
+++ topTweets.py
@@ -15,15 +15,20 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-# fetch tweets 
 
-public_tweets = api.search_tweets(q='bitcoin', until='2022-01-09', count=50, result_type='popular')
-
-# create dataframe 
-cols = ['Time', 'User', 'Retweets', 'Likes', 'Tweet']
+until_pretense = '2022-03-'
+date = 1
 data = [] 
-for tweet in public_tweets: 
-    data.append([tweet.created_at, tweet.user.screen_name, tweet.retweet_count, tweet.favorite_count, tweet.text.encode('utf-8')])
+
+for i in range(date,20):
+    
+    # fetch tweets 
+    public_tweets = api.search_tweets(q='bitcoin', until=until_pretense + str(i), count=100, result_type='popular')
+
+    # create dataframe 
+    cols = ['Time', 'User', 'Retweets', 'Likes', 'Tweet']
+    for tweet in public_tweets: 
+        data.append([tweet.created_at, tweet.user.screen_name, tweet.retweet_count, tweet.favorite_count, tweet.text.encode('utf-8')])
 
 df = pd.DataFrame(data, columns=cols)
 
